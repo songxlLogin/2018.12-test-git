83a8e51b60c014a454f46975339f3189fd967701
songxl <1229348133@qq.com>
2018-12-02 23:07:37 +0800

    创建测试版本的文件

---
+++ src/test/java/com/songxl/test/TestVersionReback.java
@@ -0,0 +1,8 @@
+package com.songxl.test;
+
+public class TestVersionReback {
+    public static void main(String[] args) {
+        System.out.println("test first");
+        System.out.println("test second");
+    }
+}
