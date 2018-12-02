cdd587c83552f37153d6dfc9ba4b1e6a9b3f7981
songxlLogin <42489609+songxlLogin@users.noreply.github.com>
2018-12-02 23:01:12 +0800

    Update TestGit.java

--- src/test/java/com/songxl/test/TestGit.java
+++ src/test/java/com/songxl/test/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("first:test git file");
-        System.out.println("second: just a println 2");
+        System.out.println("second: just a println");
     }
 }
