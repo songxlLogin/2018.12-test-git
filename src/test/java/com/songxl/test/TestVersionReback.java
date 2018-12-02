9e5b9b514ef48a8113f01ed1b07e27c281be881b
songxl <1229348133@qq.com>
2018-12-02 23:08:37 +0800

    提交bug文件

--- src/test/java/com/songxl/test/TestVersionReback.java
+++ src/test/java/com/songxl/test/TestVersionReback.java
@@ -4,5 +4,7 @@ public class TestVersionReback {
     public static void main(String[] args) {
         System.out.println("test first");
         System.out.println("test second");
+
+        System.out.println("test error");
     }
 }
